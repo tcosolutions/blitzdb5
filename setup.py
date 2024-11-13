4917a08b746a0d58d72d9b4d727dc7fb5a34e152
Marcin Kozlowski <marcinguy@gmail.com>
2024-11-13 11:38:50 +0100

    Update

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ Key Features
 
 setup(
     name='blitzdb5',
-    version='4.0.15',
+    version='4.0.16',
     license='MIT',
     url='https://github.com/tcosolutions/blitzdb3',
     packages=find_packages(),
