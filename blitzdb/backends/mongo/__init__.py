ff3b597f34822e1139948c8dcc5decdbaf1d1bce
Marcin Kozlowski <marcinguy@gmail.com>
2024-11-19 17:37:38 +0100

    Update __init__.py

--- blitzdb/backends/mongo/__init__.py
+++ blitzdb/backends/mongo/__init__.py
@@ -19,5 +19,5 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from .backend import Backend
+from .base import Backend
 from .queryset import QuerySet
