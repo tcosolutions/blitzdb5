03322a912bd6f635ae563b887319202f49404b7a
Marcin Kozlowski <marcinguy@gmail.com>
2024-11-29 11:08:42 +0100

    fix: Imports resolution

--- blitzdb/backends/file/__init__.py
+++ blitzdb/backends/file/__init__.py
@@ -19,7 +19,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from .base import Backend
+from ..base import Backend
 from .index import Index, NonUnique, TransactionalIndex
 from .queryset import QuerySet
 from .store import Store, TransactionalStore
